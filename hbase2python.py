efd50bc08d361fe6d897d8ad9af783d4c09b0453
bjwu <bjwu@zju.edu.cn>
2019-07-10 17:25:40 +0800

    fix bug

---
+++ hbase2python.py
@@ -0,0 +1,44 @@
+import happybase
+import pandas as pd
+
+def HBase2Python(limitation = None,start_row=None, stop_row=None,size_batch=1000):
+    # 建立python和hbase的连接
+    connection = None
+
+    def hbase_connect(table_name):
+        connection = happybase.Connection('localhost', autoconnect=False)
+        connection.open()
+        table = connection.table(table_name)
+        return table
+    try:
+    
+        table = hbase_connect('action_train_log')
+        data = table.scan(columns=['info:it','info:ty'],  limit=limitation
+                          ,row_start=start_row, row_stop=stop_row,batch_size=size_batch)
+        my_generator = ([int(k.decode('utf-8').split(',')[0]),int(v[b'info:it']), int(v[b'info:ty'])] for k, v in data)
+
+#         这一步生成列表可能会有问题，生成器能直接转换成 dataframe 吗？-->可以，如下所示
+#         my_list = list(my_generator)
+
+#         print(my_list)
+
+
+        # my_data 就是离线数据的输入源
+        my_data = pd.DataFrame.from_records(my_generator,columns = ['userid','itemid','type'])
+#         my_data.columns = ['userid','itemid','type']
+        my_data[['userid']] = my_data[['userid']].astype('int32')
+        my_data[['itemid']] = my_data[['itemid']].astype('int32')
+        my_data[['type']] = my_data[['type']].astype('int8')
+#         print(my_data.dtypes)
+
+
+    finally:
+        if connection:
+            connection.close()
+            
+    return my_data
+
+
+if __name__ == '__main__':
+    HBase2Python(10).dtypes
+#     print(hbase2python.HBase2Python(100).dtypes)
--- main_online.py
+++ main_online.py
@@ -4,6 +4,11 @@ import numpy as np
 import pickle
 import gc
 import redis
+import socket
+import threading
+import time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 jdata_user = pd.read_csv('./raw/jdata_user.csv',sep=',')
 jdata_product = pd.read_csv('./raw/jdata_product.csv',sep=',')
@@ -16,6 +21,7 @@ r_80 = redis.Redis(host='localhost', port=6380, db=0)
 ### TODO: 判断user或者item没在set中
 def items2ffm(user:int, itemset, mapper, valid_cols:list):
     df_test = pd.DataFrame(itemset, columns=['sku_id'])
+    tmp = df_test['sku_id']
     df_test['user_id'] = user
     df_test = df_test.merge(jdata_product,on='sku_id',how='left')
     df_test = df_test.merge(jdata_user,on='user_id',how='left')
@@ -24,6 +30,7 @@ def items2ffm(user:int, itemset, mapper, valid_cols:list):
     # 进行categocial过程
     for c in df_test.columns:
         df_test[c] = df_test[c].astype(mapper[c]['dtype']).astype(int) + mapper[c]['base']
+    df_test['item_id'] = tmp   ### 这一行是推荐行，保存原始item id
     return df_test
 
 ### 在线CF
@@ -38,18 +45,66 @@ def items2items(items_rtn, k):
     item_out = []
     import random
     for i in idx:
-        item_out.extend(r.zrange('brand:'+str(int(i)), 0, k))
+        item_out.extend(r_80.zrange('brand:'+str(int(i)), 0, k))
     return {int(i) for i in random.sample(item_out, k)}
+
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome!!!\n')
+
+    cnt = 0
+    while True:
+        data = sock.recv(1024)
+        try:
+            user, item = data.decode('UTF-8').strip().split(' ')
+            assert item.isdigit()
+            assert user.isdigit()
+        except:
+            error = "please give a btye object like b'userid itemid', thanks"
+            sock.send(error.encode())
+            continue
+
+        items_before = [int(i[:-2]) for i in r_79.lrange("latest:"+ str(int(user)), 0, 9)]
+
+        ### TODO:应该把用户信息存在redis中
+        city = int(jdata_user[jdata_user['user_id']==int(user)].city.values[0])
+        age = int(jdata_user[jdata_user['user_id']==int(user)].age.values[0])
+
+        ### 得到city和age的items candidates
+
+        items_rtn_city = {int(i) for i in r_80.zrange('city:'+ str(int(user)), 0, 10, desc=True)}
+        items_rtn_age = {int(i) for i in r_80.zrange('age:'+ str(int(user)), 0, 10, desc=True)}
+
+        ### 得到hottest candidiates
+        item_rtn_hot = {int(i) for i in r_80.zrange('hottest_one_day_before', 0, 10, desc=True)}
+
+        ### 得到CF的items candidates
+        item_rtn_cf = items2items(items_before, 10)
+
+        ### 整合items
+        item_rtn = items_rtn_city | items_rtn_age | item_rtn_hot | item_rtn_cf
+
+        ### items2ffm
+        valid_cols = ['age','sex','city','county','brand','shop_id','cate','user_id','sku_id']
+        ffm = items2ffm(int(user), list(item_rtn), cate_memo, valid_cols)
+        
+        towrite = str(user) + ':\n'
+        for item_ffm in ffm.iterrows():
+            towrite += '\t'.join([str(a) for a in list(item_ffm[1].values)])
+            towrite += '\n'
+        try:
+            sock.send(towrite.encode())
+        except:
+            break
+    sock.close()
+    print('Connection from %s:%s closed.' % addr)
 
 if __name__ == '__main__':
     
-    user = 1
-    item = 1
-    
-    items_before = [int(i[:-2]) for i in r_79.lrange("latest:"+user, 0, 9)]
-    ### TODO:应该把用户信息存在redis中
-    city = jdata_user[jdata_user['user_id']==int(user)].city.values[0]
-    age = jdata_user[jdata_user['user_id']==int(user)].age.values[0]
+    ### 打开端口
+    s.bind(('localhost', 8022))
+    s.listen(5)
+    print('Waiting for connection...')
     
     ### 得到cate_memo 和 SIM
     f = open('./model/cate_memo.pkl', 'rb')
@@ -58,20 +113,12 @@ if __name__ == '__main__':
     f = open('./model/brand_sim_metrix.pkl', 'rb')
     SIM_brand_list = pickle.load(f)
     
-    ### 得到city和age的items candidates
-    
-    items_rtn_city = r_80.zrange('city:'+ user, 0, 10, desc=True)
-    items_rtn_age = r_80.zrange('age:'+ user, 0, 10, desc=True)
-    
-    ### 得到hottest candidiates
-    item_rtn_hot = r_80.zrange('hottest_one_day_before'+ user, 0, 10, desc=True)
-    
-    ### 得到CF的items candidates
-    item_rtn_cf = items2items(items_before, 10)
-    
-    ### 整合items
-    
-    ### items2ffm
+    while True:
+    # 接受一个新连接:
+        sock, addr = s.accept()
+        # 创建新线程来处理TCP连接:
+        t = threading.Thread(target=tcplink, args=(sock, addr))
+        t.start()
     
     ### to flink
     
